c69b2b5d728567bd85870bc14d007e943ad5db4f
ISLAM BOUADJADJ <islam.bouadjadj@etu.usthb.dz>
2025-12-05 15:19:57 +0100

    Update lancer_chemins_grille.bat

--- lancer_chemins_grille.bat
+++ lancer_chemins_grille.bat
@@ -1,5 +1,5 @@
 @echo off
 REM Ce fichier lance le programme Python chemins_grille.py depuis le même dossier.
-REM Le professeur doit avoir Python installé et double-cliquer sur ce .bat.
+
 
 python chemins_grille.py
